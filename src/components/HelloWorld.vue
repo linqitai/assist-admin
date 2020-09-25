5532325de924a71fe52a9aa7406cf20ae3210873
linqitai <514917919@qq.com>
2020-09-25 14:57:16 +0800

    lqt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation22112222113331233</a>.
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation2211222222113331233</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
